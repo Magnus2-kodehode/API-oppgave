e4d07a91c847def82dcc95307bafb0f8a401ee93
Benjamin-kodehode <benjamin2.kodehode@gmail.com>
2024-12-16 14:24:31 +0100

    rip

--- main.js
+++
@@ -1,9 +0,0 @@
-const audioElement = document.getElementById("backgroundMusic");
-
-function playMusicOnce() {
-  if (audioElement.paused) {
-    audioElement.play();
-  }
-}
-
-document.body.addEventListener("click", playMusicOnce);
