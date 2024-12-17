fd8490d028156c61bdd0be9922b3395b4224fbf2
Erlend01-Kodehode <erlend01.kodehode@gmail.com>
2024-12-17 13:11:55 +0100

    First test of API

--- main.js
+++ main.js
@@ -2,3 +2,32 @@ window.onload = function () {
   const audio = document.getElementById("background-music");
   audio.play();
 };
+
+// API
+const apiEndpoint = `https://pokeapi.co/api/v2/pokemon/`;
+
+// Fetch Func
+async function pokeSearch(pokemon) {
+  try {
+    const result = await fetch(`${apiEndpoint}${pokemon}`);
+    const data = await result.json();
+    pokeResult(data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Return Data
+function pokeResult(data) {
+  console.log(data);
+  console.log(data.name);
+  console.log(`HP ${data.stats[0].base_stat}`);
+  console.log(`ATK ${data.stats[1].base_stat}`);
+  console.log(`DEF ${data.stats[2].base_stat}`);
+  console.log(`SP. ATK ${data.stats[3].base_stat}`);
+  console.log(`SP. DEF ${data.stats[4].base_stat}`);
+  console.log(`SPD ${data.stats[5].base_stat}`);
+}
+
+// To be hooked up to a searchbar later
+pokeSearch(`pikachu`);
